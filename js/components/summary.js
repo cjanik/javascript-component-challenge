a68af0dfbb0ca3aeb9b5d0b24ebd890622d0a53d
Chris Janik <chris_janik@ziffdavis.com>
2016-11-09 12:57:32 -0800

    add basic styles to article summary

--- js/components/summary.js
+++ js/components/summary.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import Radium from 'Radium';
 
 class Summary extends React.Component {
   render() {
     return (
-      <article>
+      <article style={styles.article}>
         <header>
-          <h1 className={'headline'} dangerouslySetInnerHTML={{__html: this.props.title.rendered}} />
-          <div className={'byline'}>
-            <address className={'author'}>By <a rel={'author'} href={`https://ww2.kqed.org/perspectives/wp-json/wp/v2/users/${this.props.author}`}>{this.props.author}</a></address>
-            on <time dateTime={this.props.date}>{new Date(this.props.date).toDateString()}</time>
+          <h1 className='headline' dangerouslySetInnerHTML={{__html: this.props.title.rendered}} />
+          <div className='byline'>
+            <address className='author' style={styles.address}>By <a rel='author' href={`https://ww2.kqed.org/perspectives/wp-json/wp/v2/users/${this.props.author}`}>{this.props.author}</a></address>
+            {' on '}<time dateTime={this.props.date}>{new Date(this.props.date).toDateString()}</time>
           </div>
         </header>
-        <div className={'article-excerpt'} dangerouslySetInnerHTML={{__html: this.props.excerpt.rendered}} />
+        <div className='article-excerpt' style={styles.excerpt} dangerouslySetInnerHTML={{__html: this.props.excerpt.rendered}} />
       </article>
     );
   }
 }
 
-export default Summary;
+const styles = {
+  article: {
+    display: 'block',
+    maxWidth: '600px',
+    margin: '10px auto',
+    padding: '0 18px'
+  },
+  excerpt: {
+    paddingTop: '10px',
+    fontSize: '1.3em',
+    color: '#555'
+  },
+  address: {
+    display: 'inline'
+  }
+}
+
+export default Radium(Summary);
